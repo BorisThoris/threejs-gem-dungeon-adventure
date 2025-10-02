40e9d3e5b92204c6f6765e91ee2dd19215174224
BorisThoris <modaxxx009@yahoo.com>
2025-10-03 02:25:41 +0300

    eslint issues

--- eslint.config.js
+++ eslint.config.js
@@ -3,21 +3,27 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**', '*.config.js']
+  },
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      '@typescript-eslint': tseslint.plugin,
     },
     rules: {
       // React Hooks Rules - CRITICAL
@@ -36,4 +42,4 @@ export default defineConfig([
       'react-refresh/only-export-components': 'error',
     },
   },
-])
+]
